670359df9f8b8cf485f0e5b7fd3f52cf7d938058
wzh-star <56988607+wzh-star@users.noreply.github.com>
2020-08-11 09:54:34 +0800

    Add files via upload

--- download-deluge.sh
+++ download-deluge.sh
@@ -2,9 +2,9 @@
 apt -y update
 apt -y upgrade
 apt -y install wget
-apt-get update
-apt-get install python unzip python-twisted python-openssl python-setuptools intltool python-xdg python-chardet geoip-database python-libtorrent python-notify python-pygame python-glade2 librsvg2-common xdg-utils python-mako
-wget https://down.ymgblog.com/deluge/deluge-1.3.15.zip
+apt-get -y update
+apt-get -y install python unzip python-twisted python-openssl python-setuptools intltool python-xdg python-chardet geoip-database python-libtorrent python-notify python-pygame python-glade2 librsvg2-common xdg-utils python-mako
+wget https://github.com/wzh-star/PT/raw/master/deluge-1.3.15.zip 
 unzip deluge-1.3.15.zip
 cd deluge-1.3.15
 python setup.py build
@@ -45,5 +45,7 @@ systemctl enable deluged
 systemctl enable deluge-web
 systemctl start deluged
 systemctl start deluge-web
-echo "现在访问<http://IP地址:8112>就可以进入Deluge的WebUI.输入密码后，点击 connect 就可以连接上服务器，开始使用了.WebUI 默认密码：deluge"
+echo "现在访问http://IP地址:8112就可以进入Deluge的WebUI.
+输入密码后，点击 connect 就可以连接上服务器，开始使用了.
+WebUI的默认密码：deluge，第一次输入会提醒修改密码"
 echo "Finished!"
